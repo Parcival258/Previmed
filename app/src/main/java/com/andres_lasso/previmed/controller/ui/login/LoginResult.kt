202560bb9d2f815885eea056acbbfa185c4a0490
parvical258 <andreslasso250@gmail.com>
2025-04-10 17:21:34 -0500

    Login (inicio)

--- app/src/main/java/com/andres_lasso/previmed/controller/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package com.andres_lasso.previmed.controller.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult (
-     val success:LoggedInUserView? = null,
-     val error:Int? = null
-)
