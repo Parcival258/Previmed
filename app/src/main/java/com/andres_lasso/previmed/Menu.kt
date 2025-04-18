cfd3c610219c7b8eb05d99b4317c6ccd6fc28837
IngAlim2023 <enrique.porras@udea.edu.co>
2025-04-18 16:37:19 -0500

    Menú listo, con intent ejemplo y vista beneficiario lista para trabajar

---
+++ app/src/main/java/com/andres_lasso/previmed/Menu.kt
@@ -0,0 +1,25 @@
+package com.andres_lasso.previmed
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.andres_lasso.previmed.controller.pacientes.ViewBeneficiario
+
+class Menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu)
+
+        //Intento para ir a la vista beneficiario
+        val irBene: Button = findViewById(R.id.irBeneficiario);
+        irBene.setOnClickListener{
+            val irBen = Intent(this, ViewBeneficiario::class.java)
+            startActivity(irBen);
+        }
+    }
+}
