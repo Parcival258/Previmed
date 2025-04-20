d4de93a445f6b5f07bb73d62734e8ebcfcdf5eba
IngAlim2023 <enrique.porras@udea.edu.co>
2025-04-20 12:52:20 -0500

    NavBar Beneficiarios

--- app/src/main/java/com/andres_lasso/previmed/controller/pacientes/ViewBeneficiario.kt
+++ app/src/main/java/com/andres_lasso/previmed/controller/pacientes/ViewBeneficiario.kt
@@ -5,13 +5,49 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.replace
 import com.andres_lasso.previmed.R
+import com.andres_lasso.previmed.controller.pacientes.fragmentBeneficiario.ContratoBeneficiarioFragment
+import com.andres_lasso.previmed.controller.pacientes.fragmentBeneficiario.HomeBeneficiarioFragment
+import com.andres_lasso.previmed.controller.pacientes.fragmentBeneficiario.PagosBeneficiarioFragment
+import com.andres_lasso.previmed.controller.pacientes.fragmentBeneficiario.VisitaBeneficiarioFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ViewBeneficiario : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_view_beneficiario)
 
+        bottomNavigationView = findViewById(R.id.bottom_navigation_pac)
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.btn_home ->{
+                    replaceFragment(HomeBeneficiarioFragment())
+                    true
+                }
+                R.id.btn_visita ->{
+                    replaceFragment(VisitaBeneficiarioFragment())
+                    true
+                }
+                R.id.btn_contrato ->{
+                    replaceFragment(ContratoBeneficiarioFragment())
+                    true
+                }
+                R.id.btn_pago ->{
+                    replaceFragment(PagosBeneficiarioFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+        replaceFragment(HomeBeneficiarioFragment())
+
+    }
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.paciente_nav, fragment).commit()
     }
 }
