95455adfba98f34eade3f74b8d47302670e7c556
JEISON101 <reyesjeison2006@gmail.com>
2025-05-05 16:21:01 -0500

    cambios asesor

--- app/src/main/java/com/andres_lasso/previmed/controller/asesor/PagosView.kt
+++ app/src/main/java/com/andres_lasso/previmed/controller/asesor/PagosView.kt
@@ -1,17 +1,20 @@
 package com.andres_lasso.previmed.controller.asesor
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.andres_lasso.previmed.R
+import com.andres_lasso.previmed.databinding.ActivityPagosViewBinding
+
 
 class PagosView : AppCompatActivity() {
+    private lateinit var binding: ActivityPagosViewBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_pagos_view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -19,7 +22,13 @@ class PagosView : AppCompatActivity() {
             insets
         }
 
+        binding = ActivityPagosViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.btnIrPagosAdd.setOnClickListener{
+            val ir_pagos_add = Intent(this, PagosAdd::class.java)
+            startActivity(ir_pagos_add)
+        }
 
     }
 }
