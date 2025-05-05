a79fbdc8189e91adb07b403b05bdb9f342474e5d
IngAlim2023 <enrique.porras@udea.edu.co>
2025-05-05 15:23:21 -0500

    Cree la vista Beneficiarios

---
+++ app/src/main/java/com/andres_lasso/previmed/controller/pacientes/Beneficiarios.kt
@@ -0,0 +1,21 @@
+package com.andres_lasso.previmed.controller.pacientes
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.andres_lasso.previmed.R
+
+class Beneficiarios : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_beneficiarios)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
