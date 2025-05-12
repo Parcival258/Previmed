ba9d5cb92fda44c601ef4fe2c8cf3f0bf1872b3e
IngAlim2023 <enrique.porras@udea.edu.co>
2025-05-12 15:29:07 -0500

    Implementacion de view binding en pagos

--- app/src/main/java/com/andres_lasso/previmed/controller/pacientes/fragmentBeneficiario/PagosBeneficiarioFragment.kt
+++ app/src/main/java/com/andres_lasso/previmed/controller/pacientes/fragmentBeneficiario/PagosBeneficiarioFragment.kt
@@ -7,21 +7,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.andres_lasso.previmed.R
+import com.andres_lasso.previmed.databinding.FragmentPagosBeneficiarioBinding
 
 class PagosBeneficiarioFragment : Fragment() {
-
+    private var _binding: FragmentPagosBeneficiarioBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pagos_beneficiario, container, false)
+        _binding = FragmentPagosBeneficiarioBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val contrato: TextView = view.findViewById(R.id.numero_contrato);
+        //val contrato: TextView = view.findViewById(R.id.numero_contrato);
+        val contrato: TextView = binding.numeroContrato
 
         val numeroContrato: String = "25-94985-554"
         contrato.text = "Membresía $numeroContrato"
