b53bd8f66693904cf94f958915565105447d2194
IngAlim2023 <enrique.porras@udea.edu.co>
2025-05-12 16:44:53 -0500

    Implementacion de view binding en contratos beneficiarios

--- app/src/main/java/com/andres_lasso/previmed/controller/pacientes/fragmentBeneficiario/ContratoBeneficiarioFragment.kt
+++ app/src/main/java/com/andres_lasso/previmed/controller/pacientes/fragmentBeneficiario/ContratoBeneficiarioFragment.kt
@@ -6,17 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.andres_lasso.previmed.R
+import com.andres_lasso.previmed.databinding.FragmentContratoBeneficiarioBinding
 
 
 class ContratoBeneficiarioFragment : Fragment() {
 
+    private var _binding: FragmentContratoBeneficiarioBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contrato_beneficiario, container, false)
+        _binding = FragmentContratoBeneficiarioBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
