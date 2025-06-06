a7e1755b0cca8caaa2d7dc80bbe19035ad994571
cristian castro <ruizcastrocristianfelipe@gmail.com>
2025-06-06 10:34:16 -0500

    colores y diseño asesor

--- app/src/main/java/com/andres_lasso/previmed/controller/asesor/PagosAdd.kt
+++ app/src/main/java/com/andres_lasso/previmed/controller/asesor/PagosAdd.kt
@@ -12,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.andres_lasso.previmed.R
+import com.andres_lasso.previmed.controller.asesor.fragmentAsesor.HomeAsesorFragment
 import com.andres_lasso.previmed.controller.asesor.fragmentAsesor.PagosAsesorFragment
 import com.andres_lasso.previmed.databinding.ActivityPagosAddBinding
 import com.google.android.material.datepicker.MaterialDatePicker
@@ -24,22 +25,22 @@ class PagosAdd : AppCompatActivity() {
         binding = ActivityPagosAddBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val titulares = listOf("","Andres","Jorge","Jesus","Daniela","sofia","jeison")
-        val tipos_de_pago = listOf("","Efectivo", "Transacción", "Debito automatico")
+        val titulares = listOf("Andres","Jorge","Jesus","Daniela","sofia","jeison")
+        val tipos_de_pago = listOf("Efectivo", "Transacción", "Debito automatico")
 
-        // Crear un ArrayAdapter para el Spinner de titulares
+        // ArrayAdapter para el Spinner de titulares
         val titularAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, titulares)
         titularAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.spinnerTitular.adapter = titularAdapter
 
-        // Crear un ArrayAdapter para el Spinner de tipos de pago
+        // ArrayAdapter para el Spinner de tipos de pago
         val tipoPagoAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, tipos_de_pago)
         tipoPagoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.spinnerFormaPago.adapter = tipoPagoAdapter
 
         binding.btnSavePago.setOnClickListener{
-            val ir_pagos = Intent(this, PagosAsesorFragment::class.java)
             Toast.makeText(this, "Pago Agregado exitosamene ✔️", Toast.LENGTH_SHORT).show()
+            val ir_pagos = Intent(this, PagosAsesorFragment::class.java)
             startActivity(ir_pagos)
             finish()
         }
