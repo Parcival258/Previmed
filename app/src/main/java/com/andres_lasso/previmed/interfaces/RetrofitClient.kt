56fdef5c59c6a04c779195c1f9ac0a2addafbeaf
jesus castro <jesuscastrojaramillo346@gmail.com>
2025-08-25 17:41:36 -0500

    se le agrego retrofit y se consumio el backend que esta en render

--- app/src/main/java/com/andres_lasso/previmed/interfaces/RetrofitClient.kt
+++ app/src/main/java/com/andres_lasso/previmed/interfaces/RetrofitClient.kt
@@ -1,16 +1,18 @@
 package com.andres_lasso.previmed.interfaces
+
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:3334/"
 
-    val instance: ApiService by lazy {
+    private const val BASE_URL = "https://previmedbackend-vnbb.onrender.com/"
+
+    val instance: LoginApi by lazy {
         val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(BASE_URL) // tu backend en Render
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
-        retrofit.create(ApiService::class.java)
+        retrofit.create(LoginApi::class.java)
     }
 }
