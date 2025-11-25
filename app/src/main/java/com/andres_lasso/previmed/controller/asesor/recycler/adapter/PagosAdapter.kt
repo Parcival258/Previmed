b8c9c5d258ced54f2d1a8414fcaef16e4877b249
jesus castro <jesuscastrojaramillo346@gmail.com>
2025-11-25 14:30:21 -0500

    Cambios Visuales PagosAsesors

--- app/src/main/java/com/andres_lasso/previmed/controller/asesor/recycler/adapter/PagosAdapter.kt
+++ app/src/main/java/com/andres_lasso/previmed/controller/asesor/recycler/adapter/PagosAdapter.kt
@@ -14,6 +14,7 @@ import com.andres_lasso.previmed.model.PagoModel
 class PagosAdapter :
     RecyclerView.Adapter<PagosAdapter.PagosViewHolder>() {
 
+
     private var listaPagos: List<PagoModel> = emptyList()
     private val itemsExpandidos = mutableSetOf<Int>()
 
